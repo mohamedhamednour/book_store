4e34b787404f433984c0b2c389313c728d0b0d87
hamed <mohamedhame2ed@gmail.com>
2025-05-11 20:36:21 +0300

    Add login and register actions to UserViewSet

---
+++ userapp/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+
+
+router.register('users', views.UserViewSet, basename='users')
+
+
+urlpatterns = [
+       path("", include(router.urls)),
+
+]
