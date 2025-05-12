a052e8789b39905dc877225eab2b396d21eae854
hamed <mohamedhame2ed@gmail.com>
2025-05-12 03:35:25 +0300

    add  models book and review

--- bookapp/models.py
+++ bookapp/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from .manager import BookQuerySet
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField(null=True, blank=True)
+    description = models.TextField(blank=True)
+    objects = BookQuerySet.as_manager()
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    book = models.ForeignKey(
+        to="Book", on_delete=models.CASCADE, related_name="book_reviews"
+    )
+    user = models.ForeignKey(
+        to="userapp.User", on_delete=models.CASCADE, related_name="user_reviews"
+    )
+    rating = models.PositiveSmallIntegerField()
+    comment = models.TextField(blank=True, max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ("book", "user")
+
+    def __str__(self):
+        return f"{self.user.email} - {self.book.title} ({self.rating})"
