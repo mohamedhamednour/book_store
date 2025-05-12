b60febf3604bf0c680d637b9025d255304a22146
hamed <mohamedhame2ed@gmail.com>
2025-05-12 03:33:52 +0300

    add admin models book and reviw

--- bookapp/admin.py
+++ bookapp/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Book, Review
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ("id", "title")
+    search_fields = ("title",)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "book", "rating", "comment")
+    list_filter = ("rating", "book")
+    search_fields = ("user__email", "book__title", "comment")
