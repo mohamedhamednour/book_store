90a83f27d7254d214f5867507ac02c53c1426a31
hamed <mohamedhame2ed@gmail.com>
2025-05-13 03:14:41 +0300

    added black foramt for filter and views

--- bookapp/filters.py
+++ bookapp/filters.py
@@ -1,9 +1,10 @@
 from django_filters import rest_framework as filters
 from .models import Review
 
+
 class ReviewFilter(filters.FilterSet):
-    book = filters.NumberFilter(field_name='book__id')
+    book = filters.NumberFilter(field_name="book__id")
 
     class Meta:
         model = Review
-        fields = ['book']
+        fields = ["book"]
