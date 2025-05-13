d2525bb858013d590e7beab937c41f916dd0be98
hamed <mohamedhame2ed@gmail.com>
2025-05-13 03:33:33 +0300

    added test model book and review

---
+++ bookapp/factories.py
@@ -0,0 +1,25 @@
+import factory
+from factory import fuzzy
+from .models import Book, Review
+from userapp.factories import UserFactory  
+
+
+class BookFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Book
+
+    title = factory.Faker('sentence', nb_words=3)
+    author = factory.Faker('name')
+    published_date = factory.Faker('date_this_decade')
+    description = factory.Faker('text')
+
+
+class ReviewFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Review
+
+    book = factory.SubFactory(BookFactory)
+    user = factory.SubFactory(UserFactory) 
+    rating = fuzzy.FuzzyInteger(1, 5) 
+    comment = factory.Faker('text', max_nb_chars=500)
+    created_at = factory.Faker('date_this_year')
---
+++ bookapp/test/test_models.py
@@ -0,0 +1,49 @@
+
+
+from django.test import TestCase
+from bookapp.factories import BookFactory , ReviewFactory
+from bookapp.models import Book , Review
+from userapp.factories import UserFactory
+class BookModelTests(TestCase):
+    
+    def setUp(self):
+    
+        self.book = BookFactory.create()
+
+    def test_create_book(self):
+        self.assertEqual(self.book.title, self.book.title)
+        self.assertEqual(self.book.author, self.book.author)
+        self.assertIsInstance(self.book, Book)
+
+    def test_book_string_representation(self):
+        expected_string = self.book.title
+        self.assertEqual(str(self.book), expected_string)
+
+
+    def test_book_author_max_length(self):
+        max_length = self.book._meta.get_field('author').max_length
+        self.assertEqual(max_length, 255)
+
+
+
+class ReviewModelTests(TestCase):
+    
+    def setUp(self):
+    
+        self.book = BookFactory.create()
+        self.user = UserFactory.create_batch(3)
+
+        self.review = ReviewFactory.create(book=self.book, user=self.user[0])
+
+    def test_create_review(self):
+   
+        self.assertEqual(self.review.book, self.book)
+        self.assertEqual(self.review.user, self.user[0])
+        self.assertIsInstance(self.review, Review)
+
+    def test_review_rating_range(self):
+      
+        review = ReviewFactory.create(book=self.book, user=self.user[1], rating=5)
+        self.assertEqual(review.rating, 5)
+        
+      
